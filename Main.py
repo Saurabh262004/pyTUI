f866f122a6651940d3dbe14920129dc70e282b18
Saurabh262004 <saurabhrjadhav26@gmail.com>
2025-12-08 08:56:32 +0530

    something

--- Main.py
+++ Main.py
@@ -3,6 +3,37 @@ from typing import Callable
 def test(p1='def1', p2='def2'):
   print(f'p1: {p1}, p2: {p2}')
 
+class Arg:
+  def __init__(self, cmdLineTriggers: str | int):
+    self.cmdLineTriggers = cmdLineTriggers
+
+class Command:
+  def __init__(self, fn: Callable, args: list[Arg] | tuple[Arg], trigger: str, aliases: list[str] | tuple[str], desc: str):
+    self.fn = fn
+    self.args = args
+    self.trigger = trigger
+    self.aliases = aliases
+    self.desc = desc
+
+    self.tgrTuple = [trigger,]
+
+    self.tgrTuple.extend(aliases)
+
+    self.tgrTuple = tuple(self.tgrTuple)
+
+    self.tgrStr = f'{self.trigger}, '
+
+    for tgr in self.aliases:
+      self.tgrStr += f'{tgr}, '
+
+    self.tgrStr = self.tgrStr[:-2]
+
+  def call(self, args: dict):
+    pass
+
+  def help(self):
+    print(f'\nAliases: {self.tgrStr}\n')
+
 class TUI:
   def __init__(self, title: str, desc: str = '', prompt: str = ''):
     self.title = title
@@ -12,19 +43,16 @@ class TUI:
     self.currentInput: list[str] = []
     self.inputHistory: list[list[str]] = []
 
-    self.commands: dict[str, Callable] = {
-      'quit': self.close,
-      'q': self.close,
-      'help': self.help,
-      'h': self.help
-    }
-
-    self.commandsDescs: dict[tuple, str] = {
-      ('help', 'h'): 'Print this output.',
-      ('quit', 'q'): 'Quit TUI loop.'
-    }
+    self.commands: dict[str, Command] = {}
+    self.commandsDescs: dict[tuple, str] = {}
 
     self.maxLenCmd = 4
+
+    helpCMD = Command(self.help, (Arg(),), 'help', ('h',), 'Print this output')
+    quitCMD = Command(self.close, (Arg(),), 'quit', ('q',), 'Quit TUI loop')
+
+    self.setCommand(helpCMD)
+    self.setCommand(quitCMD)
 
   @staticmethod
   def getProcessedInput(tuiInput: str) -> list[str]:
@@ -42,7 +70,7 @@ class TUI:
       cmd = self.currentInput[0]
 
       if cmd in self.commands:
-        self.commands[cmd]()
+        self.commands[cmd].fn()
       else:
         print(f'"{cmd}" is not a recognized command')
 
@@ -65,34 +93,21 @@ class TUI:
     for k, v in self.commandsDescs.items():
       ln = max(self.maxLenCmd + 3, 20)
 
-      cmds = ''
-
-      for cmd in k:
-        cmds += f'{cmd}, '
-
-      cmds = cmds[:-2]
-
-      print(f'{cmds:<{ln}}: {v}')
+      print(f'{self.commands[k[0]].tgrStr:<{ln}}: {v}')
 
     print()
 
-  def setCommand(self, trigger: str | list[str], fn: Callable, desc: str) -> bool:
-    if len(trigger) < 1:
-      return False
+  def setCommand(self, cmd: Command) -> bool:
+    self.commandsDescs[cmd.tgrTuple] = cmd.desc
 
-    if isinstance(trigger, str):
-      trigger = (trigger,)
-
-    for tgr in trigger:
+    for tgr in cmd.tgrTuple:
       self.maxLenCmd = max(self.maxLenCmd, len(tgr))
-
-    self.commandsDescs[trigger] = desc
-
-    for tgr in trigger:
-      self.commands[tgr] = fn
+      self.commands[tgr] = cmd
 
 myTUI = TUI('TUI','A test TUI app', 'input: ')
 
-myTUI.setCommand(('test', 't'), test, 'A test function.')
+myTUI.setCommand(Command(test, (Arg(),), 'test', ('t',), 'A test function.'))
+
+myTUI.commands['h'].help()
 
 myTUI.open()
